6c66ac758d12d64dffd76fd693f6cea38c57052e
uchenma <uchenm@seas.upenn.edu>
2019-08-01 19:22:59 -0400

    cover page and some other stuff

---
+++ src/Cover.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const Cover = function() {
+  return (
+    <div className="coverPage">
+      <h1>OUR NAME</h1>
+      {/* <Route path='/signup'component={Signup}/>
+        <Route path='/login'component={Login}/> */}
+      <div className="buttonContainer">
+        <Link to="/signup">
+          <button className="button buttonBlue">Sign Up</button>
+        </Link>
+        <Link to="login">
+          <button className="button buttonGreen">Log in</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Cover;
