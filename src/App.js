989cbfcb0842e5df7444ce4d999058680a09047c
uchenma <uchenm@seas.upenn.edu>
2019-08-01 19:51:08 -0400

    cover page move

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
-import Cover from './Cover'
+import Cover from './containers/Cover'
 
 function App() {
   return (
