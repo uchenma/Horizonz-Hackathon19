b2c6727bbe17d7ad4cfcf67eb4edbe2e1cdece55
git push -u origin <barbara.ogyasi@gmail.com>
2019-08-01 23:07:58 -0400

    initial styling on timeline

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-
-import Profile from "../components/Profile"
-// import Timeline from "../components/Timeline"
+import Profile from "../components/Profile";
+import Timeline from "../components/Timeline";
 // import Messages from "../components/Messages"
 
-function Dashboard(){
-
-    return(
-        <div>
-            <Profile />
-            {/* <Timeline />*/}
-        </div>
-      )
+function Dashboard() {
+  return (
+    <div>
+      <Profile />
+      <Timeline />
+    </div>
+  );
 }
 
 export default Dashboard;
